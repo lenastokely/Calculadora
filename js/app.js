3a66bf98b05e0213d9b7d233935cc64136c750ff
AnaLorenaDiaz <ss.lorena1495@gmail.com>
2018-10-25 01:07:36 -0500

    creando valores

---
+++ js/app.js
@@ -0,0 +1,14 @@
+var calculadora = {};
+
+calculadora = ( function(){
+
+  //variables
+  var pantalla;//la pantalla de resultados 
+  var valor;//almacena los valores luego de las operaciones
+  var valor1;
+  var valor2;
+  var potencia;
+  var operacion;
+  var pulsada;//cuando presionas la tecla
+
+}());
